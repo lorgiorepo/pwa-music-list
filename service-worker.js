089d4d341b04f3334b0c0fccc562e295fe8e8dd4
Lorgio Trinidad <lorgio.trinidad@globant.com>
2017-04-03 14:37:54 -0300

    Service Worker fetch event

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,7 @@ var CACHE_NAME = 'cache-v1'; //Cache Name
 
 //Files to cache
 var filesToCache = [
+	'/',
 	'./index.html',
 	'./index.html?utm=homescreen', //Query strings are treated as seperate page
 	'./css/styles.css',
@@ -31,78 +32,14 @@ self.addEventListener('activate', function (event) {
 //Adding 'fetch' event listener
 
 self.addEventListener('fetch', function(event) {
-	console.log('WORKER: fetch event in progress.');
-
-	if (event.request.method !== 'GET') {
-    /* If we don't block the event as shown below, then the request will go to
-       the network as usual.
-    */
-    console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
-    return;
-  }
-
   event.respondWith(
     caches.match(event.request)
-      .then(function(cached) {
-
-				var networked = fetch(event.request)
-          // We handle the network request with success and failure scenarios.
-          .then(fetchedFromNetwork, unableToResolve)
-          // We should catch errors on the fetchedFromNetwork handler as well.
-          .catch(unableToResolve);
-
-				console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
-        return cached || networked;
-
-				function fetchedFromNetwork(response) {
-          /* We copy the response before replying to the network request.
-             This is the response that will be stored on the ServiceWorker cache.
-          */
-          var cacheCopy = response.clone();
-
-          console.log('WORKER: fetch response from network.', event.request.url);
-
-          caches
-            // We open a cache to store the response for this request.
-            .open(version + 'pages')
-            .then(function add(cache) {
-              /* We store the response for this request. It'll later become
-                 available to caches.match(event.request) calls, when looking
-                 for cached responses.
-              */
-              cache.put(event.request, cacheCopy);
-            })
-            .then(function() {
-              console.log('WORKER: fetch response stored in cache.', event.request.url);
-            });
-
-          // Return the response so that the promise is settled in fulfillment.
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
           return response;
         }
-
-				function unableToResolve () {
-          /* There's a couple of things we can do here.
-             - Test the Accept header and then return one of the `offlineFundamentals`
-               e.g: `return caches.match('/some/cached/image.png')`
-             - You should also consider the origin. It's easier to decide what
-               "unavailable" means for requests against your origins than for requests
-               against a third party, such as an ad provider
-             - Generate a Response programmaticaly, as shown below, and return that
-          */
-
-          console.log('WORKER: fetch request failed in both cache and network.');
-
-          /* Here we're creating a response programmatically. The first parameter is the
-             response body, and the second one defines the options for the response.
-          */
-          return new Response('<h1>Service Unavailable</h1>', {
-            status: 503,
-            statusText: 'Service Unavailable',
-            headers: new Headers({
-              'Content-Type': 'text/html'
-            })
-          });
-        }
+        return fetch(event.request);
       }
     )
   );
