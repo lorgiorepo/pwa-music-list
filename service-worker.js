c5184294bd19b9e8568e0562a71a07b3e4463800
Lorgio Trinidad <lorgio.trinidad@globant.com>
2017-04-05 14:15:01 -0300

    Modificacion push notifications

--- service-worker.js
+++ service-worker.js
@@ -44,3 +44,36 @@ self.addEventListener('fetch', function(event) {
     )
   );
 });
+
+
+// triggered everytime, when a push notification is received.
+self.addEventListener('push', function(event) {
+
+  console.info('Event: Push');
+
+  var title = 'New commit on Github Repo: RIL';
+
+  var body = {
+    'body': 'Click to see the latest commit',
+    'tag': 'pwa',
+    'icon': './images/48x48.png'
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, body)
+  );
+});
+
+
+self.addEventListener('notificationclick', function(event) {
+
+  var url = './latest.html';
+
+  event.notification.close(); //Close the notification
+
+  // Open the app and navigate to latest.html after clicking the notification
+  event.waitUntil(
+    clients.openWindow(url)
+  );
+
+});
